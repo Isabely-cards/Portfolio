723e6bde9cf1e65046d950bf28ff70584e9b0bff
Isabely Cardoso Figueiredo <112524798+isabely-prog@users.noreply.github.com>
2023-05-25 19:25:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 
 //icon navBar
-let menuIcon = document.querySelector('#menu-icon');
-let navBar = document.querySelector('#navBar');
+let menuIcon = document.querySelector('#menu-icon')
+let navBar = document.querySelector('#navBar')
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navBar.classList.toggle('active')
 }
-
-
-
-
 
 //scroll sections
 
@@ -20,22 +16,26 @@ let navLinks = document.querySelector('header nav a')
 window.onscroll = () => {
 
     sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
+        let top = window.scrollY
+        let offset = sec.offsetTop - 100
+        let height = sec.offsetHeight
         let id = sec.getAttribute('id')
 
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active')
-            document.querySelector('header nav a[href*=`+ id +`]').classList.add('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
+            sec.classList.add('show-animate')
+        }else {
+            sec.classList.remove('show-animate')
+
         }
     })
 
 
 
-    let header = document.querySelector('header');
+    let header = document.querySelector('header')
 
     header.classList.toggle('sticky', window.scrollY > 100)
 
