93615d21612c93cd3f0460cbfb53ea856f1be6da
Isabely Cardoso Figueiredo <112524798+isabely-prog@users.noreply.github.com>
2023-05-25 19:42:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
 
 //icon navBar
-let menuIcon = document.querySelector('#menu-icon')
-let navBar = document.querySelector('#navBar')
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('#navBar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navBar.classList.toggle('active')
-}
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+};
 
 //scroll sections
 
-let sections = document.querySelector('section')
-let navLinks = document.querySelector('header nav a')
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
 
 window.onscroll = () => {
 
-    sections.forEach(sec => {
-        let top = window.scrollY
-        let offset = sec.offsetTop - 100
-        let height = sec.offsetHeight
-        let id = sec.getAttribute('id')
+    sections.forEach (sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            })
-            sec.classList.add('show-animate')
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            sec.classList.add('show-animate');
         }else {
-            sec.classList.remove('show-animate')
+            sec.classList.remove('show-animate');
 
-        }
-    })
+        };
+    });
 
+    let header = document.querySelector('header');
 
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-    let header = document.querySelector('header')
-
-    header.classList.toggle('sticky', window.scrollY > 100)
-
-    menuIcon.classList.remove('bx-x')
-    navBar.classList.remove('active')
+    menuIcon.classList.remove('bx-x');
+    navBar.classList.remove('active');
 }
